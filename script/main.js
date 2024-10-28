2a26e4ccbb8deb6c8a2620f5860595de13236cff
Snoobd <89268858+Snoobd@users.noreply.github.com>
2024-10-28 13:17:59 -0300

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -71,7 +71,7 @@ document.getElementById('btn').addEventListener('click', () => {
     count_click++
     if(count_click <= 1) {
         setInterval(create_heart, 600) 
-        typewriterParagraph()
+        setTimeout(typewriterParagraph, 14100)
         setTimeout(typewriterSpan, 46000)
         animatedPlay()
 
